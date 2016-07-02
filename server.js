556d19b101904d4c1467393ab0253468fe7f5039
Natalie Akam <natalie.akam@gmail.com>
2016-07-02 16:02:41 +0100

    Adds create-post read and write functions

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import fs from 'fs';
 const app = express();
 
 app.use(express.static("public"));
@@ -7,6 +8,27 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/create-post', (req, res) => {
+	function readData(callback) {
+		fs.readFile(__dirname + '/data/posts.json', (err, file) => {
+			if(err) {
+				console.log(err.message);
+			}
+			let parsedFile = JSON.parse(file);
+			const datestamp = new Date().valueOf().toString();
+			parsedFile[datestamp] = req.body.blogpost;
+			callback(parsedFile);
+		});
+	}
+
+	function writeData(file) {
+		const stringifiedFile = JSON.stringify(file);
+		fs.writeFile(__dirname + '/data/posts.json', stringifiedFile, () => {
+			console.log('Post added');
+		});
+	}
+
+	readData(writeData);
+
 	res.redirect('/');
 });
 
